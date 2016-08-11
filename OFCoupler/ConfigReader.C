2bcf8ca9cc7d680fd39bed8cbe449685aa3d92fa
Lucía Cheung <cheunglucia@gmail.com>
2016-08-11 13:09:45 +0200

    Separate read and write data

--- OFCoupler/ConfigReader.C
+++ OFCoupler/ConfigReader.C
@@ -15,11 +15,24 @@ ofcoupler::ConfigReader::ConfigReader(std::string configFile, std::string partic
         for(int j = 0; j < configInterfaces[i]["patch-names"].size(); j++) {
             interface.patchNames.push_back(configInterfaces[i]["patch-names"][j].as<std::string>());
         }
-        for(int j = 0; j < configInterfaces[i]["data"].size(); j++) {
-            struct Data data;
-            data.name = configInterfaces[i]["data"][j]["name"].as<std::string>();
-            data.direction = configInterfaces[i]["data"][j]["direction"].as<std::string>();
-            interface.data.push_back(data);
+        if(configInterfaces[i]["write-data"]) {
+            for(int j = 0; j < configInterfaces[i]["write-data"].size(); j++) {
+                interface.writeData.push_back(configInterfaces[i]["write-data"][j].as<std::string>());
+            }
+        }
+        if(configInterfaces[i]["read-data"]) {
+            for(int j = 0; j < configInterfaces[i]["read-data"].size(); j++) {
+                interface.readData.push_back(configInterfaces[i]["read-data"][j].as<std::string>());
+            }
+        }
+        // For solvers using the old config file format:
+        if(configInterfaces[i]["data"]) {
+            for(int j = 0; j < configInterfaces[i]["data"].size(); j++) {
+                struct Data data;
+                data.name = configInterfaces[i]["data"][j]["name"].as<std::string>();
+                data.direction = configInterfaces[i]["data"][j]["direction"].as<std::string>();
+                interface.data.push_back(data);
+            }
         }
         _interfaces.push_back(interface);
     }
