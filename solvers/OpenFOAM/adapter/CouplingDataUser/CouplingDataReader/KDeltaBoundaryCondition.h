6bd271bcdf8ac23846873ed37bf1678c91dda7bf
Lucía Cheung <cheunglucia@gmail.com>
2016-09-05 13:25:51 +0200

    Turn KDelta to template

--- solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/KDeltaBoundaryCondition.h
+++ solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/KDeltaBoundaryCondition.h
@@ -9,18 +9,50 @@
 
 namespace ofcoupler
 {
+template<typename autoPtrTurb>
 class KDeltaBoundaryCondition : public CouplingDataReader
 {
 protected:
     volScalarField & _T;
-    autoPtr<compressible::turbulenceModel> & _turbulence;
+    autoPtrTurb & _turbulence;
 public:
-    KDeltaBoundaryCondition(volScalarField & T, autoPtr<compressible::turbulenceModel> & _turbulence);
+    
+    KDeltaBoundaryCondition(volScalarField &T, autoPtrTurb &_turbulence) : 
+        _T(T),
+        _turbulence(_turbulence)
+    {
+        
+    }
     
     // CouplingDataReader interface
 public:
-    void read(double *dataBuffer);
-};
-}
+    
+    void read(double *dataBuffer)
+    {
+        
+        int bufferIndex = 0;
+    
+        for(int k = 0; k < _patchIDs.size(); k++) {
+    
+            int patchID = _patchIDs.at(k);
+            
+            const fvPatch & kPatch = refCast<const fvPatch>(_turbulence->kappaEff()().mesh().boundary()[patchID]);
+            scalarField myKDelta = _turbulence->kappaEff()().boundaryField()[patchID] * kPatch.deltaCoeffs();
+            
+            mixedFvPatchScalarField & TPatch = refCast<mixedFvPatchScalarField>(_T.boundaryField()[patchID]);
+            forAll(TPatch, i) {
+                double nbrKDelta = dataBuffer[bufferIndex++];
+                TPatch.valueFraction()[i] = nbrKDelta / (myKDelta[i] + nbrKDelta);
+//                std::cout << "read valueFraction(" << i << ") = " << TPatch.valueFraction()[i] << std::endl;
+            }
+    
+        }
+        
+    }
+
+    
+}; // end class
+
+} // end namespace
 
 #endif // KDELTABOUNDARYCONDITION_H
