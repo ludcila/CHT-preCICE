bf3dacab688b3263cbecc760dcfd95228a03557e
Lucia Cheung Yau <lcheung@simscale.com>
2016-09-05 13:25:51 +0200

    Turn KDelta to template

--- solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataWriter/KDeltaBoundaryValues.h
+++ solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataWriter/KDeltaBoundaryValues.h
@@ -8,20 +8,55 @@
 namespace ofcoupler
 {
 
+template<typename autoPtrTurb>
 class KDeltaBoundaryValues : public CouplingDataWriter
 {
 protected:
-    autoPtr<compressible::turbulenceModel> & _turbulence;
+    autoPtrTurb & _turbulence;
     
 public:
-    KDeltaBoundaryValues(autoPtr<compressible::turbulenceModel> & turbulence);
+    
+    KDeltaBoundaryValues(autoPtrTurb & turbulence) :
+        _turbulence(turbulence)
+    {
+    }
     
     
     // CouplingDataWriter interface
 public:
-    void write(double *dataBuffer);
-};
+    
+    void write(double * dataBuffer)
+    {
+        
+        int bufferIndex = 0;
+        for(int k = 0; k < _patchIDs.size(); k++) {
+    
+            int patchID = _patchIDs.at(k);
+    
+            const fvPatch & kPatch = refCast<const fvPatch>(_turbulence->kappaEff()().mesh().boundary()[patchID]);
+            
+            /*
+            Info << "kappa" << _turbulence->kappaEff()() << endl;
+            Info << "alpha" << _turbulence->alphaEff()() << endl;
+            */
+            
+            scalarField kDelta = _turbulence->kappaEff()().boundaryField()[patchID] * kPatch.deltaCoeffs();
+    
+            std::cout << "KDelta" << std::endl;
+            forAll(kDelta, i) {
+                dataBuffer[bufferIndex++] = kDelta[i];
+//                std::cout << i << ") " << "write kDelta = " << kDelta[i] << std::endl;
+            }
+    
+        }
+    }
+    
+}; // end class
 
-}
+} // end namespace ofcoupler
+
+
+
+
 
 #endif // KDELTABOUNDARYVALUES_H
