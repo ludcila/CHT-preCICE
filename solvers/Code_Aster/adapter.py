c856eed537fc8d6f2b7d073998983911d6ff92f4
Lucia Cheung Yau <lcheung@simscale.com>
2016-09-08 13:30:32 +0200

    Update code aster adapter

--- solvers/Code_Aster/adapter.py
+++ solvers/Code_Aster/adapter.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import numpy.linalg
 from mpi4py import MPI
+from Cata.cata import *
 from Utilitai.partition import *
 
 precice_root = os.getenv('PRECICE_ROOT')
@@ -12,11 +13,52 @@ sys.path.insert(0, precice_python_adapter_root)
 import PySolverInterface
 from PySolverInterface import *
 
+class Adapter:
+	
+	conductivity = 100
+	precice = None
+	numInterfaces = 0
+	interfaces = []
+	LOADS = []
+	MESH = None
+	MODEL = None
+	
+	def __init__(self, precice, config, MESH, MODEL):
+		self.numInterfaces = len(config)
+		self.precice = precice
+		self.MESH = MESH
+		self.MODEL = MODEL
+		self.configure(config)
+	
+	def configure(self, config):
+		L = [None] * self.numInterfaces
+		for i in range(self.numInterfaces):
+			interface = Interface(self.precice, config[i], self.MESH, self.MODEL)
+			BCs = interface.createBCs()
+			L[i] = AFFE_CHAR_THER(MODELE=self.MODEL, ECHANGE=BCs)
+			interface.setLoad(L[i])
+			interface.setConductivity(np.ones(interface.writeDataSize) * self.conductivity)
+			self.LOADS.append({"CHARGE": L[i]})
+			self.interfaces.append(interface)
+	
+	def sendCouplingData(self, TEMP, dt=0, NUME_ORDRE=0):
+		if (dt == 0) or (dt > 0 and self.precice.isWriteDataRequired(dt)):
+			for interface in self.interfaces:
+				interface.writeBCs(TEMP, NUME_ORDRE)
+	
+	def receiveCouplingData(self):
+		if self.precice.isReadDataAvailable():
+			for interface in self.interfaces:
+				interface.readAndUpdateBCs()
+				
+	def setConductivity(self, cond):
+		self.conductivity = cond
+
 class Interface:
 	
 	precice = None
 	
-	mesh = MAIL_PY()
+	mesh = None
 	
 	facesGroupName = ""
 	nodesGroupName = ""
@@ -52,25 +94,30 @@ class Interface:
 	readDataSize = 0
 	writeDataSize = 0
 	
+	MESH = None
+	MODEL = None
 	LOAD = None
+	LOADS = []
 	
-	def __init__(self, precice):
+	def __init__(self, precice, names, MESH, MODEL):
 		self.precice = precice
+		self.MESH = MESH
+		self.MODEL = MODEL
+		self.mesh = MAIL_PY()
+		self.mesh.FromAster(MESH)
+		self.configure(names)
 	
-	def configure(self, data):
-		self.nodesGroupName = data["nodesGroupName"]
-		self.facesGroupName = data["facesGroupName"]
-		self.nodesMeshName = data["nodesMeshName"]
-		self.faceCentersMeshName = data["faceCentersMeshName"]
+	def configure(self, names):
+		self.nodesGroupName = names["nodesGroupName"]
+		self.facesGroupName = names["facesGroupName"]
+		self.nodesMeshName = names["nodesMeshName"]
+		self.faceCentersMeshName = names["faceCentersMeshName"]
 		self.nodes = [self.mesh.correspondance_noeuds[idx] for idx in self.mesh.gno[self.nodesGroupName]]
 		self.faces = [self.mesh.correspondance_mailles[idx] for idx in self.mesh.gma[self.facesGroupName]]
 		self.nodeCoordinates = [self.mesh.cn[idx] for idx in self.mesh.gno[self.nodesGroupName]]
 		connectivity = [self.mesh.co[idx] for idx in self.mesh.gma[self.facesGroupName]]
-		self.faceCenterCoordinates = [(
-			self.mesh.cn[connectivity[i][0]] + 
-			self.mesh.cn[connectivity[i][1]] + 
-			self.mesh.cn[connectivity[i][2]] ) / 3 
-			for i in range(len(connectivity))]
+		self.faceCenterCoordinates = np.array([np.array([self.mesh.cn[node] for node in elem]).mean(0) for elem in connectivity])
+		self.setNormals()
 		self.setVertices()
 		self.setDataIDs()
 		
@@ -79,6 +126,18 @@ class Interface:
 		
 		self.readHCoeff = np.zeros(self.readDataSize)
 		self.readTemp = np.zeros(self.readDataSize)
+	
+	def setNormals(self):
+		# Get normals at the nodes
+		N = CREA_CHAMP(
+			MODELE=self.MODEL,
+			TYPE_CHAM='NOEU_GEOM_R',
+			GROUP_MA=self.facesGroupName,
+			OPERATION='NORMALE'
+		)
+		self.normals = N.EXTR_COMP(lgno=[self.nodesGroupName]).valeurs
+		self.normals = np.resize(np.array(self.normals), (len(self.normals)/3, 3))
+		DETRUIRE(CONCEPT={"NOM": N})
 	
 	def setVertices(self):
 		# Nodes
@@ -119,9 +178,6 @@ class Interface:
 	
 	def getFaceCenterCoordinates(self):
 		return self.faceCenterCoordinates
-	
-	def setNormals(self, normals):
-		self.normals = normals
 		
 	def getNormals(self):
 		return self.normals
@@ -139,11 +195,8 @@ class Interface:
 		]
 		return BCs
 	
+	
 	def updateBCs(self, temp, hCoeff):
-		
-		if self.LOAD is None:
-			print "ERROR: LOAD not defined. Call setAsterLoad(LOAD) first, before trying to update the BCs!"
-			exit(1)
 			
 		self.LOAD.sdj.CHTH.T_EXT.VALE.changeJeveuxValues(len(temp),
 										tuple(np.array(range(len(temp))) * 10 + 1),
@@ -161,31 +214,50 @@ class Interface:
 		self.precice.readBlockScalarData(self.readTempDataID, self.readDataSize, self.preciceFaceCenterIndices, self.readTemp)
 		self.updateBCs(self.readTemp, self.readHCoeff)
 		
-	def writeBCs(self, T, Q):
-		writeTemp, writeHCoeff = self.getBoundaryValues(T, Q)
+	def writeBCs(self, TEMP, NUME_ORDRE=0):
+		writeTemp, writeHCoeff = self.getBoundaryValues(TEMP, NUME_ORDRE)
 		self.precice.writeBlockScalarData(self.writeHCoeffDataID, self.writeDataSize, self.preciceNodeIndices, writeHCoeff)
 		self.precice.writeBlockScalarData(self.writeTempDataID, self.writeDataSize, self.preciceNodeIndices, writeTemp)
 
-	def getBoundaryValues(self, T, Q):
-		if self.normals is None:
-			print "ERROR: Call setNormals(normals) before calling getBoundaryValues()!"
-			exit(1)
+	def getBoundaryValues(self, TEMP, NUME_ORDRE=0):
 		if self.conductivity is None:
 			print "ERROR: Call setConductivity(conductivity) before calling getBoundaryValues()!"
 			exit(1)
+			
+		# Temperature at the nodes
+		T = CREA_CHAMP(
+			RESULTAT=TEMP,
+			NOM_CHAM='TEMP',
+			TYPE_CHAM='NOEU_TEMP_R',
+			OPERATION='EXTR',
+			NUME_ORDRE=NUME_ORDRE,
+		)
+		
+		# Flux at the nodes
+		Q = CREA_CHAMP(
+			RESULTAT=TEMP,
+			NOM_CHAM='FLUX_NOEU',
+			TYPE_CHAM='NOEU_FLUX_R',
+			OPERATION='EXTR',
+			NUME_ORDRE=NUME_ORDRE,
+		)
+		
 		t = T.EXTR_COMP(lgno=[self.nodesGroupName]).valeurs
 		q = Q.EXTR_COMP(lgno=[self.nodesGroupName]).valeurs
 		q = np.resize(np.array(q), (len(q)/3, 3))
-		# Surface normal flux
+		
+		DETRUIRE(CONCEPT=({'NOM': T}, {'NOM': Q}))
+		
+		# Surface normal flux (flux dot normal)
 		snq = (self.normals * q).sum(1)
 		# Sink temperature
 		writeTemp = t + snq * self.delta
 		# Heat transfer coefficient
 		writeHCoeff = np.array(self.conductivity) / self.delta
 		return writeTemp, writeHCoeff
+	
+	def setLoad(self, LOAD):
+		self.LOAD = LOAD
 		
 	def setConductivity(self, conductivity):
 		self.conductivity = conductivity
-
-	def setAsterLoad(self, LOAD):
-		self.LOAD = LOAD
