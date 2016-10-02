fefae8a3bc1b83c17b7b7640daa48be3b97fc128
Lucia Cheung Yau <lcheung@simscale.com>
2016-10-02 13:30:29 +0200

    Refactor code

--- utilities/preciceautoconf/schemes.py
+++ utilities/preciceautoconf/schemes.py
@@ -1,9 +1,11 @@
 from lxml import etree
 
+
 class CouplingScheme(object):
-    def __init__(self, timestep, maxTime, participants, serial=False):
-        self.timestep = timestep
-        self.maxTime = maxTime
+    
+    def __init__(self, time_step, max_time, participants, serial=False):
+        self.time_step = time_step
+        self.max_time = max_time
         self.participants = participants
         self.serial = serial
         if serial:
@@ -12,64 +14,63 @@ class CouplingScheme(object):
             self.serialOrParallel = "parallel"
         self.explicitOrImplicit = "explicit" # Base class is explicit
 
-    def getCouplingSchemeName(self):
+    def get_coupling_scheme_name(self):
         return self.serialOrParallel + "-" + self.explicitOrImplicit
 
-    def addCouplingSchemeTagTo(self, parent):
-        couplingSchemeTag = etree.SubElement(parent, etree.QName("coupling-scheme", self.getCouplingSchemeName()))
-        self.addTimestepTagTo(couplingSchemeTag)
-        self.addMaxTimeTagTo(couplingSchemeTag)
-        self.addCouplingParticipantTagsTo(couplingSchemeTag)
-        self.addExchangeTagsTo(couplingSchemeTag)
-        return couplingSchemeTag
+    def add_coupling_scheme_tag_to(self, parent):
+        coupling_scheme_tag = etree.SubElement(parent, etree.QName("coupling-scheme", self.get_coupling_scheme_name()))
+        self.add_time_step_tag_to(coupling_scheme_tag)
+        self.add_max_time_tag_to(coupling_scheme_tag)
+        self.add_coupling_participant_tags_to(coupling_scheme_tag)
+        self.add_exchange_tags_to(coupling_scheme_tag)
+        return coupling_scheme_tag
 
-    def addTimestepTagTo(self, parent):
-        etree.SubElement(parent, "timestep-length", value=str(self.timestep))
+    def add_time_step_tag_to(self, parent):
+        etree.SubElement(parent, "time_step-length", value=str(self.time_step))
 
-    def addMaxTimeTagTo(self, parent):
-        etree.SubElement(parent, "max-time", value=str(self.maxTime))
+    def add_max_time_tag_to(self, parent):
+        etree.SubElement(parent, "max-time", value=str(self.max_time))
 
-    def addCouplingParticipantTagsTo(self, parent):
+    def add_coupling_participant_tags_to(self, parent):
         etree.SubElement(parent, "participants", first=self.participants[0].name, second=self.participants[1].name)
 
-    def addExchangeTagsTo(self, parent):
-        interfaces = self.participants[0].getInterfacesWith(self.participants[1])
+    def add_exchange_tags_to(self, parent):
+        interfaces = self.participants[0].get_interfaces_with(self.participants[1])
         for interface in interfaces:
             # First participant initializes the coupling data of the second participant only in a parallel scheme
-            interface.addExchangeTagsTo(parent, initialize=not self.serial)
+            interface.add_exchange_tags_to(parent, initialize=not self.serial)
             # Second participant initializes the coupling data of the first participant
-            interface.partnerInterface.addExchangeTagsTo(parent, initialize=True)
-            interface.addPostProcessingDataTagsTo(parent)
+            interface.partner_interface.add_exchange_tags_to(parent, initialize=True)
+            interface.add_post_processing_data_tags_to(parent)
 
-    def addM2nTagTo(self, parent, type="sockets"):
+    def add_m2n_tag_to(self, parent, type="sockets"):
         e = etree.SubElement(parent, etree.QName("m2n", type), to=self.participants[1].name)
         e.set("from", self.participants[0].name)
-        if self.participants[0].domainDecomposed or self.participants[1].domainDecomposed:
+        if self.participants[0].domain_decomposed or self.participants[1].domain_decomposed:
             e.set("distribution-type", "scatter-gather")
 
 
 class ImplicitCouplingScheme(CouplingScheme):
 
-    def __init__(self, timestep, maxTime, maxIterations, participants, serial=False):
-        super(ImplicitCouplingScheme, self).__init__(timestep, maxTime, participants, serial)
+    def __init__(self, time_step, max_time, max_iterations, participants, serial=False):
+        super(ImplicitCouplingScheme, self).__init__(time_step, max_time, participants, serial)
         self.explicitOrImplicit = "implicit"
-        self.maxIterations = maxIterations
+        self.max_iterations = max_iterations
 
-    def addCouplingSchemeTagTo(self, parent):
+    def add_coupling_scheme_tag_to(self, parent):
         # If there are multiple interfaces between the two participants,
         # we generate a coupling-scheme tag for each interface,
         # otherwise we will be able to apply post-processing only to one data
-        interfaces = self.participants[0].getInterfacesWith(self.participants[1])
+        interfaces = self.participants[0].get_interfaces_with(self.participants[1])
         for interface in interfaces:
-            couplingSchemeTag = super(ImplicitCouplingScheme, self).addCouplingSchemeTagTo(parent)
-            interface.addExchangeTagsTo(couplingSchemeTag)
-            interface.partnerInterface.addExchangeTagsTo(couplingSchemeTag)
-            self.addMaxIterationsTagTo(couplingSchemeTag)
-            postProcessingTag = self.addPostProcessingTagTo(couplingSchemeTag)
-            self.addPostProcessingDataTagsTo(postProcessingTag)
+            coupling_scheme_tag = super(ImplicitCouplingScheme, self).add_coupling_scheme_tag_to(parent)
+            interface.add_exchange_tags_to(coupling_scheme_tag)
+            interface.partner_interface.add_exchange_tags_to(coupling_scheme_tag)
+            self.addmax_iterationsTagTo(coupling_scheme_tag)
+            self.add_post_processing_tag_to(coupling_scheme_tag)
 
-    def addExchangeTagsTo(self, parent):
-        # Override addExchangeTagsTo from CouplingScheme which puts the exchange tags
+    def add_exchange_tags_to(self, parent):
+        # Override add_exchange_tags_to from CouplingScheme which puts the exchange tags
         # from all interface pairs under the same coupling-scheme tag
         # For implicit coupling we will have a separate coupling-scheme tag for
         # each interface pair, so that post-processing can be applied to all the data
@@ -78,14 +79,15 @@ class ImplicitCouplingScheme(CouplingScheme):
     def getRelativeConvergenceMeasureTagsTo(self):
         pass
 
-    def addMaxIterationsTagTo(self, parent):
-        print self.maxIterations
-        etree.SubElement(parent, "max-iterations", value=str(self.maxIterations))
+    def addmax_iterationsTagTo(self, parent):
+        print self.max_iterations
+        etree.SubElement(parent, "max-iterations", value=str(self.max_iterations))
 
-    def addPostProcessingTagTo(self, parent):
-        return etree.SubElement(parent, etree.QName("post-processing", "IQN-ILS"))
+    def add_post_processing_tag_to(self, parent):
+        post_processing = IQNILSPostProcessing()
+        post_processing.add_post_processing_tag_to(parent)
 
-    def addPostProcessingDataTagsTo(self, parent):
+    def add_post_processing_data_tags_to(self, parent):
         if self.serial:
             pass
             # Apply post-processing only to the data from the second participant
@@ -99,40 +101,65 @@ class ImplicitCouplingScheme(CouplingScheme):
 
 class MultiCouplingScheme(ImplicitCouplingScheme):
 
-    def __init__(self, timestep, maxTime, maxIterations, participantPairs, serial=False):
-        self.participantPairs = participantPairs
+    def __init__(self, time_step, max_time, max_iterations, participant_pairs, serial=False):
+        self.participant_pairs = participant_pairs
         self.participants = []
-        for pair in self.participantPairs:
+        for pair in self.participant_pairs:
             self.participants.append(pair[0])
             self.participants.append(pair[1])
         self.participants = list(set(self.participants))
-        super(MultiCouplingScheme, self).__init__(timestep, maxTime, maxIterations, self.participants, False)
+        super(MultiCouplingScheme, self).__init__(time_step, max_time, max_iterations, self.participants, False)
         self.schemeName="multi"
 
-    def getCouplingSchemeName(self):
+    def get_coupling_scheme_name(self):
         return "multi"
 
-    def addCouplingSchemeTagTo(self, parent):
-        couplingSchemeTag = super(ImplicitCouplingScheme, self).addCouplingSchemeTagTo(parent)
-        self.addMaxIterationsTagTo(couplingSchemeTag)
-        postProcessingTag = self.addPostProcessingTagTo(couplingSchemeTag)
-        self.addPostProcessingDataTagsTo(postProcessingTag)
+    def add_coupling_scheme_tag_to(self, parent):
+        coupling_scheme_tag = super(ImplicitCouplingScheme, self).add_coupling_scheme_tag_to(parent)
+        self.addmax_iterationsTagTo(coupling_scheme_tag)
+        self.add_post_processing_tag_to(coupling_scheme_tag)
 
-    def addCouplingParticipantTagsTo(self, parent):
+    def add_coupling_participant_tags_to(self, parent):
         for participant in self.participants:
             etree.SubElement(parent, "participant", name=participant.name)
 
-    def addExchangeTagsTo(self, parent):
-        for participants in self.participantPairs:
-            interfaces = participants[0].getInterfacesWith(participants[1])
+    def add_exchange_tags_to(self, parent):
+        for participants in self.participant_pairs:
+            interfaces = participants[0].get_interfaces_with(participants[1])
             for interface in interfaces:
-                interface.addExchangeTagsTo(parent, initialize=True)
-                interface.partnerInterface.addExchangeTagsTo(parent, initialize=True)
-                interface.addPostProcessingDataTagsTo(parent)
+                interface.add_exchange_tags_to(parent, initialize=True)
+                interface.partner_interface.add_exchange_tags_to(parent, initialize=True)
 
-    def addM2nTagTo(self, parent, type="sockets"):
-        for participants in self.participantPairs:
+    def add_m2n_tag_to(self, parent, type="sockets"):
+        for participants in self.participant_pairs:
             e = etree.SubElement(parent, etree.QName("m2n", type), to=participants[1].name)
             e.set("from", participants[0].name)
-            if participants[0].domainDecomposed or participants[1].domainDecomposed:
+            if participants[0].domain_decomposed or participants[1].domain_decomposed:
                 e.set("distribution-type", "scatter-gather")
+
+
+class IQNILSPostProcessing:
+
+    def __init__(self, initial_relaxation=1.0, max_used_iterations=100):
+        self.initial_relaxation = initial_relaxation
+        self.max_used_iterations = max_used_iterations
+
+    def add_post_processing_tag_to(self, parent):
+        e = etree.SubElement(parent, etree.QName("post-processing", "IQN-ILS"))
+        self.add_initial_relaxation_tag_to(e)
+        self.add_max_used_iterations_tag(e)
+        self.add_filter_tag_to(e)
+        self.add_preconditioner_tag_to(e)
+        return e
+
+    def add_max_used_iterations_tag(self, parent):
+        etree.SubElement(parent, "max-used-iterations", value=str(self.max_used_iterations))
+
+    def add_initial_relaxation_tag_to(self, parent):
+        etree.SubElement(parent, "initial-relaxation", value=str(self.initial_relaxation))
+
+    def add_preconditioner_tag_to(self, parent):
+        etree.SubElement(parent, "preconditioner", type="residual-sum")
+
+    def add_filter_tag_to(self, parent):
+        etree.SubElement(parent, "filter", type="QR1", limit=str(1e-6))
