223e8378b3e370a80e9988dcbcc5772e338cff02
Lucía Cheung <cheunglucia@gmail.com>
2016-10-02 22:45:49 +0200

    Add data post processing tags

--- utilities/preciceautoconf/schemes.py
+++ utilities/preciceautoconf/schemes.py
@@ -11,13 +11,13 @@ class CouplingScheme(object):
         self.participants = participants
         self.is_serial = coupling_config.is_serial()
         if self.is_serial:
-            self.serialOrParallel = "serial"
+            self.serial_or_parallel = "serial"
         else:
-            self.serialOrParallel = "parallel"
+            self.serial_or_parallel = "parallel"
         self.explicit_or_implicit = "explicit" # Base class is explicit
 
     def get_coupling_scheme_name(self):
-        return self.serialOrParallel + "-" + self.explicit_or_implicit
+        return self.serial_or_parallel + "-" + self.explicit_or_implicit
 
     def add_coupling_scheme_tag_to(self, parent):
         coupling_scheme_tag = etree.SubElement(parent, etree.QName("coupling-scheme", self.get_coupling_scheme_name()))
@@ -43,7 +43,6 @@ class CouplingScheme(object):
             interface.add_exchange_tags_to(parent, initialize=not self.is_serial)
             # Second participant initializes the coupling data of the first participant
             interface.partner_interface.add_exchange_tags_to(parent, initialize=True)
-            interface.add_post_processing_data_tags_to(parent)
 
     def add_m2n_tag_to(self, parent, type="sockets"):
         e = etree.SubElement(parent, etree.QName("m2n", type), to=self.participants[1].name)
@@ -68,8 +67,8 @@ class ImplicitCouplingScheme(CouplingScheme):
             coupling_scheme_tag = super(ImplicitCouplingScheme, self).add_coupling_scheme_tag_to(parent)
             interface.add_exchange_tags_to(coupling_scheme_tag)
             interface.partner_interface.add_exchange_tags_to(coupling_scheme_tag)
-            self.addmax_iterationsTagTo(coupling_scheme_tag)
-            self.add_post_processing_tag_to(coupling_scheme_tag)
+            self.add_max_iterations_tag_to(coupling_scheme_tag)
+            self.add_post_processing_tag_to(coupling_scheme_tag, interface)
 
     def add_exchange_tags_to(self, parent):
         # Override add_exchange_tags_to from CouplingScheme which puts the exchange tags
@@ -81,24 +80,18 @@ class ImplicitCouplingScheme(CouplingScheme):
     def get_relative_convergence_measures(self):
         pass
 
-    def addmax_iterationsTagTo(self, parent):
+    def add_max_iterations_tag_to(self, parent):
         print self.max_iterations
         etree.SubElement(parent, "max-iterations", value=str(self.max_iterations))
 
-    def add_post_processing_tag_to(self, parent):
-        post_processing = IQNILSPostProcessing()
-        post_processing.add_post_processing_tag_to(parent)
-
-    def add_post_processing_data_tags_to(self, parent):
+    def add_post_processing_tag_to(self, parent, interface=None):
+        post_processing = IQNILSPostProcessing(self.coupling_config)
+        post_processing_tag = post_processing.add_post_processing_tag_to(parent)
         if self.is_serial:
-            pass
-            # Apply post-processing only to the data from the second participant
-            # etree.SubElement(parent, "data", name=self.participants[1].dataNameT, mesh=self.participants[1].writeMesh) # check
+            post_processing.add_post_processing_data_tags_to(post_processing_tag, interface.partner_interface)
         else:
-            pass
-            # Apply post-processing to data from both participants
-            # etree.SubElement(parent, "data", name=self.participants[0].dataNameT, mesh=self.participants[0].writeMesh) # check
-            # etree.SubElement(parent, "data", name=self.participants[1].dataNameT, mesh=self.participants[1].writeMesh) # check
+            post_processing.add_post_processing_data_tags_to(post_processing_tag, interface.interface)
+            post_processing.add_post_processing_data_tags_to(post_processing_tag, interface.partner_interface)
 
 
 class MultiCouplingScheme(ImplicitCouplingScheme):
@@ -112,13 +105,16 @@ class MultiCouplingScheme(ImplicitCouplingScheme):
         self.participants = list(set(self.participants))
         super(MultiCouplingScheme, self).__init__(coupling_config, self.participants)
         self.scheme_name = "multi"
+        self.serial_or_parallel = "parallel"
 
     def get_coupling_scheme_name(self):
         return "multi"
 
     def add_coupling_scheme_tag_to(self, parent):
+        # In multi-coupling, there is only one coupling-scheme tag, which contains all data exchanges,
+        # and only one post-processing sub-tag
         coupling_scheme_tag = super(ImplicitCouplingScheme, self).add_coupling_scheme_tag_to(parent)
-        self.addmax_iterationsTagTo(coupling_scheme_tag)
+        self.add_max_iterations_tag_to(coupling_scheme_tag)
         self.add_post_processing_tag_to(coupling_scheme_tag)
 
     def add_coupling_participant_tags_to(self, parent):
@@ -139,10 +135,22 @@ class MultiCouplingScheme(ImplicitCouplingScheme):
             if participants[0].domain_decomposed or participants[1].domain_decomposed:
                 e.set("distribution-type", "scatter-gather")
 
+    def add_post_processing_tag_to(self, parent, interface=None):
+        post_processing = IQNILSPostProcessing(self.coupling_config)
+        post_processing_tag = post_processing.add_post_processing_tag_to(parent)
+        for participants_pair in self.participant_pairs:
+            interfaces = participants_pair[0].get_interfaces_with(participants_pair[1])
+            for interface in interfaces:
+                post_processing.add_post_processing_data_tags_to(post_processing_tag, interface)
+                post_processing.add_post_processing_data_tags_to(post_processing_tag, interface.partner_interface)
+
 
 class IQNILSPostProcessing:
 
-    def __init__(self, initial_relaxation=1.0, max_used_iterations=100):
+    def __init__(self, coupling_config, interface=None, participant_pairs=None, initial_relaxation=1.0, max_used_iterations=100):
+        self.interface = interface
+        self.participant_pairs = participant_pairs
+        self.coupling_config = coupling_config
         self.initial_relaxation = initial_relaxation
         self.max_used_iterations = max_used_iterations
 
@@ -165,3 +173,6 @@ class IQNILSPostProcessing:
 
     def add_filter_tag_to(self, parent):
         etree.SubElement(parent, "filter", type="QR1", limit=str(1e-6))
+
+    def add_post_processing_data_tags_to(self, parent, interface):
+        etree.SubElement(parent, "data", name=interface.participant.data_name_T, mesh=interface.write_mesh)
