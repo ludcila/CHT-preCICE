4711df667b663e9fb5cd0a3c19e02c224840f61e
Lucía Cheung <cheunglucia@gmail.com>
2016-10-06 12:34:28 +0200

    Add class for checkpointing

---
+++ solvers/OpenFOAM/adapter/Checkpoint.h
@@ -0,0 +1,33 @@
+#ifndef CHECKPOINT_H
+#define CHECKPOINT_H
+
+#include <vector>
+#include "fvCFD.H"
+
+namespace ofcoupler
+{
+class Checkpoint
+{
+protected:
+    scalar _couplingIterationTimeValue;
+    label _couplingIterationTimeIndex;
+    std::vector<volScalarField*> _volScalarFields;
+    std::vector<volScalarField*> _volScalarFieldCopies;
+    std::vector<volVectorField*> _volVectorFields;
+    std::vector<volVectorField*> _volVectorFieldCopies;
+    std::vector<surfaceScalarField*> _surfaceScalarFields;
+    std::vector<surfaceScalarField*> _surfaceScalarFieldCopies;
+    Foam::Time & _runTime;
+    void _storeTime();
+    void _reloadTime();
+public:
+    Checkpoint(Foam::Time & runTime);
+    void addVolScalarField(volScalarField & field);
+    void addVolVectorField(volVectorField & field);
+    void addSurfaceScalarField(surfaceScalarField & field);
+    void write();
+    void read();
+};
+}
+
+#endif // CHECKPOINT_H
