35f62694ff0f975fab28ee5287a7f05782c2d2e9
Lucia Cheung Yau <lcheung@simscale.com>
2016-10-19 23:18:58 +0200

    Format code

--- solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/KDeltaBoundaryCondition.h
+++ solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/KDeltaBoundaryCondition.h
@@ -13,44 +13,47 @@ template<typename autoPtrTurb>
 class KDeltaBoundaryCondition : public CouplingDataReader
 {
 protected:
-    volScalarField & _T;
-    autoPtrTurb & _turbulence;
-public:
-    
-    KDeltaBoundaryCondition(volScalarField &T, autoPtrTurb &_turbulence) : 
-        _T(T),
-        _turbulence(_turbulence)
-    {
-        
-    }
-    
-    // CouplingDataReader interface
-public:
-    
-    void read(double *dataBuffer)
-    {
-        
-        int bufferIndex = 0;
-    
-        for(uint k = 0; k < _patchIDs.size(); k++) {
-    
-            int patchID = _patchIDs.at(k);
-            
-            const fvPatch & kPatch = refCast<const fvPatch>(_turbulence->kappaEff()().mesh().boundary()[patchID]);
-            scalarField myKDelta = _turbulence->kappaEff()().boundaryField()[patchID] * kPatch.deltaCoeffs();
-            
-            mixedFvPatchScalarField & TPatch = refCast<mixedFvPatchScalarField>(_T.boundaryField()[patchID]);
-            forAll(TPatch, i) {
-                double nbrKDelta = dataBuffer[bufferIndex++];
-                TPatch.valueFraction()[i] = nbrKDelta / (myKDelta[i] + nbrKDelta);
-//                std::cout << "read valueFraction(" << i << ") = " << TPatch.valueFraction()[i] << std::endl;
-            }
-    
-        }
-        
-    }
 
-    
+	volScalarField & _T;
+	autoPtrTurb & _turbulence;
+
+public:
+
+	KDeltaBoundaryCondition( volScalarField & T, autoPtrTurb & _turbulence ) :
+		_T( T ),
+		_turbulence( _turbulence )
+	{
+
+	}
+
+	// CouplingDataReader interface
+
+public:
+
+	void read( double * dataBuffer )
+	{
+
+		int bufferIndex = 0;
+
+		for( uint k = 0 ; k < _patchIDs.size() ; k++ )
+		{
+
+			int patchID = _patchIDs.at( k );
+
+			const fvPatch & kPatch = refCast<const fvPatch>( _turbulence->kappaEff() ().mesh().boundary()[patchID] );
+			scalarField myKDelta = _turbulence->kappaEff() ().boundaryField()[patchID] * kPatch.deltaCoeffs();
+
+			mixedFvPatchScalarField & TPatch = refCast<mixedFvPatchScalarField>( _T.boundaryField()[patchID] );
+			forAll( TPatch, i )
+			{
+				double nbrKDelta = dataBuffer[bufferIndex++];
+				TPatch.valueFraction()[i] = nbrKDelta / ( myKDelta[i] + nbrKDelta );
+//                std::cout << "read valueFraction(" << i << ") = " << TPatch.valueFraction()[i] << std::endl;
+			}
+
+		}
+	}
+
 }; // end class
 
 } // end namespace
