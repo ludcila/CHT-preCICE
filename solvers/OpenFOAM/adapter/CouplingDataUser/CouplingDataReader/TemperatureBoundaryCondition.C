5664b60a2be88844740484cdfaad5b886f99914c
Lucia Cheung Yau <lcheung@simscale.com>
2016-11-21 16:38:50 +0100

    Clean up temperature BC

--- solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/TemperatureBoundaryCondition.C
+++ solvers/OpenFOAM/adapter/CouplingDataUser/CouplingDataReader/TemperatureBoundaryCondition.C
@@ -1,34 +1,22 @@
 #include "TemperatureBoundaryCondition.h"
 
-adapter::TemperatureBoundaryCondition::TemperatureBoundaryCondition(volScalarField &T) :
-    _T(T)
+adapter::TemperatureBoundaryCondition::TemperatureBoundaryCondition( volScalarField & T ) :
+	_T( T )
 {
 
 }
 
-void adapter::TemperatureBoundaryCondition::read(double * dataBuffer)
+void adapter::TemperatureBoundaryCondition::read( double * dataBuffer )
 {
+	int bufferIndex = 0;
 
-    int bufferIndex = 0;
-
-    for(uint k = 0; k < _patchIDs.size(); k++) {
-
-        int patchID = _patchIDs.at(k);
-
-        std::cout << patchID << std::endl;
-
-        int size = _T.boundaryField()[patchID].size();
-
-        std::cout << size << std::endl;
-
-        scalarField temperatureField(size); // TODO: avoid this?
-        forAll(temperatureField, i) {
-            temperatureField[i] = dataBuffer[bufferIndex++];
-        }
-        refCast<fixedValueFvPatchScalarField>(_T.boundaryField()[patchID]) == temperatureField;
-
-        // Info << "Temperature" << temperatureField << endl;
-
-    }
-
+	for( uint k = 0 ; k < _patchIDs.size() ; k++ )
+	{
+		int patchID = _patchIDs.at( k );
+		forAll( _T.boundaryField()[patchID], i )
+		{
+			_T.boundaryField()[patchID][i] = dataBuffer[bufferIndex++];
+		}
+	}
 }
+
