b30f651a93c85767241fd5bae055edee06ff6b59
Lucia Cheung Yau <lcheung@simscale.com>
2016-12-16 15:20:20 +0100

    Handle read/write data uniformly

--- utilities/preciceautoconf/interface.py
+++ utilities/preciceautoconf/interface.py
@@ -28,10 +28,10 @@ class Interface(object):
 
     def add_mesh_tags_to(self, parent):
         mesh = etree.SubElement(parent, "mesh", name=self.mesh)
-        etree.SubElement(mesh, "use-data", name=self.participant.data_name_T)
-        etree.SubElement(mesh, "use-data", name=self.participant.data_name_HTC)
-        etree.SubElement(mesh, "use-data", name=self.partner_interface.participant.data_name_T)
-        etree.SubElement(mesh, "use-data", name=self.partner_interface.participant.data_name_HTC)
+        for write_data in self.write_data:
+            etree.SubElement(mesh, "use-data", name=write_data)
+        for read_data in self.partner_interface.write_data:
+            etree.SubElement(mesh, "use-data", name=read_data)
 
     def add_provide_mesh_tags_to(self, parent):
         etree.SubElement(parent, "use-mesh", name=self.read_mesh, provide="yes")
@@ -39,55 +39,58 @@ class Interface(object):
             etree.SubElement(parent, "use-mesh", name=self.write_mesh, provide="yes")
 
     def add_from_mesh_tags_to(self, parent):
-        e = etree.SubElement(parent, "use-mesh", name=self.write_mesh)
+        e = etree.SubElement(parent, "use-mesh", name=self.write_mesh, decomposition="pre-filter-post-filter")
         e.set("from", self.participant.name)
 
     def add_read_write_mapping_tags_to(self, parent):
-        etree.SubElement(parent, "write-data", name=self.participant.data_name_T, mesh=self.write_mesh)
-        etree.SubElement(parent, "write-data", name=self.participant.data_name_HTC, mesh=self.write_mesh)
-        etree.SubElement(parent, "read-data", name=self.partner_interface.participant.data_name_T, mesh=self.read_mesh)
-        etree.SubElement(parent, "read-data", name=self.partner_interface.participant.data_name_HTC, mesh=self.read_mesh)
+        for write_data in self.write_data:
+            etree.SubElement(parent, "write-data", name=write_data, mesh=self.write_mesh)
+        for read_data in self.partner_interface.write_data:
+            etree.SubElement(parent, "read-data", name=read_data, mesh=self.read_mesh)
         e = etree.SubElement(parent, etree.QName("mapping", "nearest-neighbor"),
                              direction="read", to=self.read_mesh, constraint="consistent")
         e.set("from", self.partner_interface.write_mesh)
 
     def add_exchange_tags_to(self, parent, initialize=False):
         # From me to partner
-        e = etree.SubElement(parent, "exchange", data=self.participant.data_name_T, mesh=self.write_mesh)
-        e.set("from", self.participant.name)
-        e.set("to", self.partner_interface.participant.name)
-        if initialize:
-            e.set("initialize", "yes")
-        e = etree.SubElement(parent, "exchange", data=self.participant.data_name_HTC, mesh=self.write_mesh)
-        e.set("from", self.participant.name)
-        e.set("to", self.partner_interface.participant.name)
-        if initialize:
-            e.set("initialize", "yes")
+        for write_data in self.write_data:
+            e = etree.SubElement(parent, "exchange", data=write_data, mesh=self.write_mesh)
+            e.set("from", self.participant.name)
+            e.set("to", self.partner_interface.participant.name)
+            if initialize:
+                e.set("initialize", "yes")
+
 
 class OpenFOAMInterface(Interface):
 
     def __init__(self, participant, name=None):
         super(OpenFOAMInterface, self).__init__(participant, name)
+        self.write_data = ["Sink-Temperature-" + self.participant.name,
+                           "Heat-Transfer-Coefficient-" + self.participant.name]
 
 
 class CalculiXInterface(Interface):
 
     def __init__(self, participant, name=None):
         super(CalculiXInterface, self).__init__(participant, name)
+        self.write_data = ["Sink-Temperature-" + self.participant.name,
+                           "Heat-Transfer-Coefficient-" + self.participant.name]
 
 
 class CodeAsterInterface(Interface):
 
     def __init__(self, participant, name=None):
         super(CodeAsterInterface, self).__init__(participant, name)
+        self.write_data = ["Sink-Temperature-" + self.participant.name,
+                           "Heat-Transfer-Coefficient-" + self.participant.name]
 
     def add_mesh_tags_to(self, parent):
         write_mesh = etree.SubElement(parent, "mesh", name=self.write_mesh)
-        etree.SubElement(write_mesh, "use-data", name=self.participant.data_name_T)
-        etree.SubElement(write_mesh, "use-data", name=self.participant.data_name_HTC)
+        for write_data in self.write_data:
+            etree.SubElement(write_mesh, "use-data", name=write_data)
         read_mesh = etree.SubElement(parent, "mesh", name=self.read_mesh)
-        etree.SubElement(read_mesh, "use-data", name=self.partner_interface.participant.data_name_T)
-        etree.SubElement(read_mesh, "use-data", name=self.partner_interface.participant.data_name_HTC)
+        for read_data in self.partner_interface.write_data:
+            etree.SubElement(read_mesh, "use-data", name=read_data)
 
     def set_mesh_names(self):
         # For Robin-Robin coupling, Code_Aster has its read-data at the faces
