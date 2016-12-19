8bfa6909fd919db15d9edd262bee1ee5ff1de1d6
Lucia Cheung Yau <lcheung@simscale.com>
2016-12-19 11:58:08 +0100

    Add reference temperature profile

--- examples/buoyantPimpleFoam-ccx-FRSR/run.sh
+++ examples/buoyantPimpleFoam-ccx-FRSR/run.sh
@@ -10,3 +10,7 @@ PIDSolid=$!
 # Wait for the run to finish
 wait $PIDFluid
 wait $PIDSolid
+
+diff convergence-Fluid.txt convergence-Fluid.txt.ref
+sample -case Fluid -time 1
+diff Fluid/postProcessing/sets/1/line_T.xy line_T.xy
