266b0fbc82f04ce8a5d2111bf3a1a957ba68ace8
Lucia Cheung Yau <lcheung@simscale.com>
2017-01-04 12:26:12 +0100

    Add some error messages to the CalculiX ConfigReader

--- solvers/CalculiX/adapter/ConfigReader.hpp
+++ solvers/CalculiX/adapter/ConfigReader.hpp
@@ -8,11 +8,18 @@
 #ifndef CONFIGREADER_HPP
 #define CONFIGREADER_HPP
 
+
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
 extern "C" {
 
 	#include "ConfigReader.h"
 
 }
 
+void ConfigReader_CheckFields( YAML::Node & config, std::string participantName );
 
 #endif
