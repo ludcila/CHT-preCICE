7d3c83df8ec18e6a72a5c55223f49f4924c935f3
Lucía Cheung <cheunglucia@gmail.com>
2017-01-11 00:33:45 +0100

    Update run.sh of example coupling

--- examples/buoyantPimpleFoam-ccx-FRSR/run.sh
+++ examples/buoyantPimpleFoam-ccx-FRSR/run.sh
@@ -11,6 +11,8 @@ PIDSolid=$!
 wait $PIDFluid
 wait $PIDSolid
 
-diff convergence-Fluid.txt convergence-Fluid.txt.ref
+rm diff.txt
+diff convergence-Fluid.txt convergence-Fluid.txt.ref &>> diff.txt
 sample -case Fluid -time 1
-diff Fluid/postProcessing/sets/1/line_T.xy line_T.xy
+diff Fluid/postProcessing/sets/1/line_T.xy line_T.xy &>> diff.txt
+wc -c diff.txt
